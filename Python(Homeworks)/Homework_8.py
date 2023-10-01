818d5e520e8b1bed1619b8a80fcb02c9f923eb91
Alex <16alextancura@gmail.com>
2023-10-02 00:54:10 +0300

    изменил 8е дз

--- Python(Homeworks)/Homework_8.py
+++ Python(Homeworks)/Homework_8.py
@@ -20,6 +20,28 @@ def enter_data():
     with open('book.txt', 'a', encoding = 'utf-8') as file:    
         file.write(f'{name} {surname}\n{number}\n\n')  
 
+
+def changes_data():
+    print("Выберите вариант поиска:\n"
+          '1. Имя\n'
+          '2. Фамилия\n'
+          '3. Телефон')
+    index = int(input('Введите вариант: ')) - 1 
+    searched = input('Ввести поисковые данные: ').title()
+    with open('book.txt', 'r', encoding = 'utf-8') as file:
+        data = file.read().strip().split('\n\n')
+        for item in data:          
+            new_item = item.replace('\n', ' ').split()  
+            if searched in new_item[index]:
+                print(item, end="\n\n") 
+def enter_changes_name(): 
+    return input("Введите изменения: ").title()   
+def changes():
+    change = enter_changes_name()
+    with open('book.txt', 'a', encoding = 'utf-8') as file:    
+        file.write(f'{change}\n\n') 
+print(f'Изменено на {changes}')
+                
         
 def print_data():
     with open('book.txt', 'r', encoding = 'utf-8') as file:
@@ -38,29 +60,38 @@ def search_line():
         for item in data:          
             new_item = item.replace('\n', ' ').split()  
             if searched in new_item[index]:
-                print(item, end="\n\n")            
+                print(item, end="\n\n")        
+
+# def delete_data():
+
  
 
 def interface():
     cmd = 0
-    while cmd != '4':
+    while cmd != '6':
         print("Выберите действие:\n"
             '1. Добавить контакт\n'
-            '2. Вывести все контакты\n'
-            '3. Поиск контакта\n'
-            '4. Выход')
+            '2. Изменить контакт\n'
+            '3. Вывести все контакты\n'
+            '4. Поиск контакта\n'
+            '5. Удалить контакт\n'
+            '6. Выход')
         cmd = input("введите действие: ")
-        while cmd not in ('1', '2', '3', '4'):
+        while cmd not in ('1', '2', '3', '4', '5', '6'):
             print("Некорректный ввод")
             cmd = input("Введите действие: ")
         match cmd: 
             case '1': 
                 enter_data()
             case '2':
-                print_data()
+                changes_data()
             case '3':
-                search_line()
+                print_data()
             case '4':
+                search_line()
+            # case '5':
+            #     delete_data()
+            case '6':
                 print('Всего доброго! ')
 
                 
