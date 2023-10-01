c6aa1c86358fe675521a43db777b1e13beb6af76
Alex <16alextancura@gmail.com>
2023-10-02 02:49:06 +0300

    Все добавлено

--- Python(Homeworks)/Homework_8.py
+++ Python(Homeworks)/Homework_8.py
@@ -22,25 +22,28 @@ def enter_data():
 
 
 def changes_data():
-    print("Выберите вариант поиска:\n"
-          '1. Имя\n'
-          '2. Фамилия\n'
-          '3. Телефон')
-    index = int(input('Введите вариант: ')) - 1 
-    searched = input('Ввести поисковые данные: ').title()
-    with open('book.txt', 'r', encoding = 'utf-8') as file:
-        data = file.read().strip().split('\n\n')
-        for item in data:          
-            new_item = item.replace('\n', ' ').split()  
-            if searched in new_item[index]:
-                print(item, end="\n\n") 
-def enter_changes_name(): 
-    return input("Введите изменения: ").title()   
-def changes():
-    change = enter_changes_name()
-    with open('book.txt', 'a', encoding = 'utf-8') as file:    
-        file.write(f'{change}\n\n') 
-print(f'Изменено на {changes}')
+    with open("book.txt", "r", encoding="utf-8") as data:
+        old_data = data.read()
+        print(old_data)
+        index_delete_data = int(input("Введите номер строки для удаления: ")) - 1
+        old_data_lines = old_data.split("\n")
+        edit_old_data_lines = old_data_lines[index_delete_data]
+        elements = edit_old_data_lines.split(" ")
+        first_name = enter_first_name()
+        second_name = enter_second_name()
+        phone = input(f"Введите номер телефона: {enter_phone_number}")
+        num = elements[0]
+        if len(first_name) and len(second_name) == 0:
+            first_name == elements[1]
+            second_name == elements[1]
+        if len(phone) == 0:
+            phone == elements[2]
+        edited_line = f"{num} {first_name} {second_name} {phone}"
+        old_data_lines[index_delete_data] = edited_line
+        print(f"Запись - {edit_old_data_lines}, изменена на - {edited_line}\n")
+        with open("book.txt", "w", encoding="utf-8") as file:
+            file.write("\n".join(old_data_lines))
+
                 
         
 def print_data():
@@ -62,7 +65,16 @@ def search_line():
             if searched in new_item[index]:
                 print(item, end="\n\n")        
 
-# def delete_data():
+def delete_data():
+    with open("book.txt", "r", encoding="utf-8") as data:
+        tel_book = data.read()
+        index_delete_data = int(input("Введите номер строки для удаления: \n")) - 1
+        tel_book_lines = tel_book.split("\n")
+        del_tel_book_lines = tel_book_lines[index_delete_data]
+        tel_book_lines.pop(index_delete_data)
+        print(f"Удалена запись: {del_tel_book_lines}\n")
+        with open("book.txt", "w", encoding="utf-8") as data:
+            data.write("\n".join(tel_book_lines))
 
  
 
@@ -89,8 +101,8 @@ def interface():
                 print_data()
             case '4':
                 search_line()
-            # case '5':
-            #     delete_data()
+            case '5':
+                delete_data()
             case '6':
                 print('Всего доброго! ')
 
